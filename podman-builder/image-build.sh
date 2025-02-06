da7cb2d5394ef3a0be17171486fecb2fd20cfb37
Nathan Crapo <107080899+AxellioNate@users.noreply.github.com>
2025-02-06 15:53:36 -0700

    Revert "Add podman build environment and README"
    This reverts commit 7c3953499bfe9038b15939f442d38860de5d7ffe.

--- podman-builder/image-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Use this script to build the docker image defined in this dir - only
-# need to do this once for any given host
-podman build -t libpcap-builder .
--- podman-builder/install-deps.sh
+++
@@ -1,8 +0,0 @@
-set -ex
-source /hbb/activate
-
-yum install -y \
-    bison \
-    flex \
-    wget \
-    lz4-devel
